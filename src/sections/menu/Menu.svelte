308ea2fc82780d8c60e3a20149b859b21284ad5a
Mohammed-Ali Hussein <mohammedalihussein02@gmail.com>
2023-01-26 12:54:33 +0800

    added bg darkening when opening modal

--- src/sections/menu/Menu.svelte
+++ src/sections/menu/Menu.svelte
@@ -2,18 +2,18 @@
   import closeMenu from "../../images/icon-menu-close.svg";
 
   import { createEventDispatcher } from "svelte";
-  import { fly } from "svelte/transition";
+  import { fade, fly } from "svelte/transition";
   import { circOut } from "svelte/easing"; 
 
   let dispatcher = createEventDispatcher()
-  const ANIMATION_DURATION = 500;
 
   function handleMenuClose() {
     dispatcher("closeMenu");
   }
 </script>
 
-<div in:fly="{{ x: 200, easing: circOut, opacity: 1, duration: ANIMATION_DURATION }}" out:fly="{{ x: 0, easing: circOut, opacity: 1, duration: ANIMATION_DURATION }}" class="h-screen w-8/12 absolute right-0 top-0 p-7 bg-off_white">
+<div class="w-full h-screen bg-opaque_black absolute top-0 left-0" in:fade="{{ easing: circOut, duration: 500 }}"></div>
+<div class="h-screen w-8/12 absolute right-0 top-0 p-7 bg-off_white" in:fly="{{ x: 200, easing: circOut, opacity: 1, duration: 500 }}">
   <div class="flex justify-end">
     <img src={closeMenu} alt="close-menu" class="object-fit cursor-pointer" on:click={handleMenuClose} on:keydown on:keyup on:keypress>
   </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -14,7 +14,8 @@ module.exports = {
         off_white: 'hsl(36, 100%, 99%)',
         grayish_blue: 'hsl(233, 8%, 79%)',
         dark_grayish_blue: 'hsl(236, 13%, 42%)',
-        very_dark_blue: 'hsl(240, 100%, 5%)'
+        very_dark_blue: 'hsl(240, 100%, 5%)',
+        opaque_black: 'rgba(0, 0, 0, 0.5)',
       },
       fontFamily: {
         inter: ['Inter', 'sans-serif']

e7eae084e680ea5130cf769f4a581018e10921e4
Mohammed-Ali Hussein <mohammedalihussein02@gmail.com>
2023-01-26 10:24:39 +0800

    top stories mobile done

--- src/sections/top-stories/Story.svelte
+++ src/sections/top-stories/Story.svelte
@@ -5,11 +5,11 @@
   export let image = null;
 </script>
 
-<div class="flex py-4">
-  <img src={image} alt={`${title}-image`} class="object-fit h-36">
-  <div class="flex-col items-stretch pl-5">
+<div class="flex py-4 h-36">
+  <img src={image} alt={`${title}-image`} class="object-fit h-full">
+  <div class="h-full flex flex-col justify-between pl-4">
     <p class="font-inter text-grayish_blue text-3xl font-extrabold">{index}</p>
     <p class="font-inter text-very_dark_blue text-l font-extrabold">{title}</p>
-    <p class="font-inter text-very_dark_blue text-sm tracking-wider line-4">{subText}</p>
+    <p class="font-inter text-very_dark_blue text-sm tracking-wider line-4 opacity-60">{subText}</p>
   </div>
 </div>

16596b9d84e4d8efdd8878d333047c7b992b7cd8
Mohammed-Ali Hussein <mohammedalihussein02@gmail.com>
2023-01-26 12:55:23 +0800

    added modal closing when clicking on darkened area

--- src/sections/menu/Menu.svelte
+++ src/sections/menu/Menu.svelte
@@ -12,7 +12,7 @@
   }
 </script>
 
-<div class="w-full h-screen bg-opaque_black absolute top-0 left-0" in:fade="{{ easing: circOut, duration: 500 }}"></div>
+<div class="w-full h-screen bg-opaque_black absolute top-0 left-0" in:fade="{{ easing: circOut, duration: 500 }}" on:click={handleMenuClose} on:keydown on:keyup on:keypress></div>
 <div class="h-screen w-8/12 absolute right-0 top-0 p-7 bg-off_white" in:fly="{{ x: 200, easing: circOut, opacity: 1, duration: 500 }}">
   <div class="flex justify-end">
     <img src={closeMenu} alt="close-menu" class="object-fit cursor-pointer" on:click={handleMenuClose} on:keydown on:keyup on:keypress>
